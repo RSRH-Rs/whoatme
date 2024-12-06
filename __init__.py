ef7c70125b6fd279458f9889f158c0ed1eabc76f
Jack <jl1171881690@gmail.com>
2024-12-05 23:25:02 -0500

    修复正则匹配规则

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ from .utils import *
 from nonebot import NoneBot
 import re
 from sqlitedict import SqliteDict
-from hoshino.config.__bot__ import NICKNAME,SUPERUSERS
+from hoshino.config.__bot__ import NICKNAME, SUPERUSERS
 from .config import *
 
 DB_PATH = get_path("data")
@@ -27,10 +27,20 @@ sv = Service(
     visible=True,  # 可见性
     enable_on_default=True,  # 默认启用
     bundle="功能",  # 分组归类
-    help_=sv_help  # 帮助说明
+    help_=sv_help,  # 帮助说明
 )
 
-@sv.on_fullmatch("谁艾特我", "谁at我", "谁AT我", "谁艾特我？", "谁At我", "谁@我", '谁@我了',"wam")
+
+@sv.on_fullmatch(
+    "谁艾特我",
+    "谁at我",
+    "谁AT我",
+    "谁艾特我？",
+    "谁At我",
+    "谁@我",
+    "谁@我了",
+    "wam",
+)
 @sv.on_rex(r"^谁.{0,3}艾特我了?$")
 async def who_at_me(bot: HoshinoBot, ev: CQEvent):
     uid = str(ev.user_id)
@@ -46,13 +56,17 @@ async def who_at_me(bot: HoshinoBot, ev: CQEvent):
     forward_messages = []
 
     for index, msg in enumerate(new_ated_user_messages):
-        converted_time = convert_stamp_time_to_date(msg['time'])
+        converted_time = convert_stamp_time_to_date(msg["time"])
         if get_now_time() - msg["time"] >= reminder_expire_time:
             del new_ated_user_messages[index]
             continue
         else:
-            if show_time and not str(msg['data']['content']).startswith(f"[{converted_time}]"):
-                msg['data']['content'] = f"[{converted_time}]\n{msg['data']['content']}"
+            if show_time and not str(msg["data"]["content"]).startswith(
+                f"[{converted_time}]"
+            ):
+                msg["data"][
+                    "content"
+                ] = f"[{converted_time}]\n{msg['data']['content']}"
             forward_messages.append(msg)
 
     if not forward_messages:
@@ -60,7 +74,19 @@ async def who_at_me(bot: HoshinoBot, ev: CQEvent):
 
     db[uid] = new_ated_user_messages
     forward_messages.reverse() if reversed_range else None
-    forward_messages.insert(0, node_custom(user_id=ev.self_id, name=nickname, content=TIPS,time=get_now_time())) if tips_header else None
+    (
+        forward_messages.insert(
+            0,
+            node_custom(
+                user_id=ev.self_id,
+                name=nickname,
+                content=TIPS,
+                time=get_now_time(),
+            ),
+        )
+        if tips_header
+        else None
+    )
 
     await bot.send_group_forward_msg(group_id=gid, messages=forward_messages)
 
@@ -69,23 +95,34 @@ async def who_at_me(bot: HoshinoBot, ev: CQEvent):
 async def message_monitor(bot: HoshinoBot, ev: CQEvent):
     gid = str(ev.group_id)
     raw_message = str(ev.raw_message)
-    replied_message = re.match(r".*?(\[CQ:reply,id=-?\d+])(\[CQ:at,qq=\d+]).*?",raw_message)
-    ated_user_ids = set(re.findall(fr"\[CQ:at,qq=(\d+)]", raw_message))
+    replied_message = re.match(
+        r".*?(\[CQ:reply,id=-?\d+])(\[CQ:at,qq=\d+]).*?", raw_message
+    )
+    ated_user_ids = set(re.findall(rf"\[CQ:at,qq=(\d+).*?]", raw_message))
     sender_user_name = await get_member_name(bot=bot, gid=gid, uid=ev.user_id)
     db = init_db("data", tablename=gid)
     if not ated_user_ids:
         return
 
     if replied_message:
-        raw_message = raw_message.replace(replied_message.group(0),replied_message.group(1))
+        raw_message = raw_message.replace(
+            replied_message.group(0), replied_message.group(1)
+        )
 
     for user_id in ated_user_ids:
         user_id = str(user_id)
         user_name = await get_member_name(bot=bot, gid=gid, uid=user_id)
-        raw_message = raw_message.replace(f"[CQ:at,qq={user_id}]", f"@{user_name}").strip()
+        raw_message = raw_message.replace(
+            f"[CQ:at,qq={user_id}]", f"@{user_name}"
+        ).strip()
 
     for user_id in ated_user_ids:
-        msg = node_custom(user_id=ev.user_id, name=sender_user_name, content=raw_message.strip(), time=get_now_time())
+        msg = node_custom(
+            user_id=ev.user_id,
+            name=sender_user_name,
+            content=raw_message.strip(),
+            time=get_now_time(),
+        )
         if user_id in db:
             # db[user_id].append(msg)不知道为什么不好使
             db_messages: list = db[user_id].copy()
@@ -99,8 +136,6 @@ async def message_monitor(bot: HoshinoBot, ev: CQEvent):
             db[user_id] = db_messages
         else:
             db[user_id] = [msg]
-
-
 
 
 @sv.on_fullmatch("清除全部艾特记录", "清除艾特db", "cadb")
